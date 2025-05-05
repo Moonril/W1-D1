f5a52b02f269068250d517c8cebc8baeebfe16be
Vincenza <vinni2690@hotmail.com>
2025-05-05 14:47:58 +0200

    initial commit

---
+++ src/Esercizio1.java
@@ -0,0 +1,8 @@
+public class Esercizio1 {
+    public static void main(String[] args){
+        System.out.println(multiply(2, 2));
+    }
+    public static double multiply(int a, int b){
+        return a+b;
+    }
+}
